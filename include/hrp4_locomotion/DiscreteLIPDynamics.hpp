fa70ba450a620f22417c13bfbfd2837bcc96277a
Michele Cipriano <micco00x@gmail.com>
2024-05-06 16:17:01 +0200

    Implementing disturbance into LIP discrete dynamics (#4)

--- include/hrp4_locomotion/DiscreteLIPDynamics.hpp
+++ include/hrp4_locomotion/DiscreteLIPDynamics.hpp
@@ -10,10 +10,11 @@ typedef Eigen::Vector3d LIPControlInput;
 class DiscreteLIPDynamics {
  public:
   DiscreteLIPDynamics(double eta, int64_t timestep_msec);
-  LIPState integrate(const LIPState& lip_state, const LIPControlInput& zmp_vel);
+  LIPState integrate(const LIPState& lip_state, const LIPControlInput& zmp_vel, const Eigen::Vector3d& disturbance=Eigen::Vector3d::Zero());
  private:
   Eigen::Vector3d updateState(const LIPState& lip_state, double zmpDot, int dim);
 
+  double timestep_;
   double eta_;
   Eigen::Matrix3d A_;
   Eigen::Vector3d B_;
--- src/DiscreteLIPDynamics.cpp
+++ src/DiscreteLIPDynamics.cpp
@@ -4,23 +4,22 @@
 namespace labrob {
 
 DiscreteLIPDynamics::DiscreteLIPDynamics(double eta, int64_t timestep_msec)
-  : eta_(eta) {
-  double timestep = 0.001 * static_cast<double>(timestep_msec);
-  double ch = cosh(eta * timestep);
-  double sh = sinh(eta * timestep);
+  : timestep_(0.001 * static_cast<double>(timestep_msec)), eta_(eta) {
+  double ch = cosh(eta * timestep_);
+  double sh = sinh(eta * timestep_);
   A_ << ch, sh / eta, 1.0 - ch, eta * sh, ch, -eta * sh, 0.0, 0.0, 1.0;
-  B_ << timestep - sh / eta, 1.0 - ch, timestep;
+  B_ << timestep_ - sh / eta, 1.0 - ch, timestep_;
 }
 
 LIPState
-DiscreteLIPDynamics::integrate(const LIPState& lip_state, const LIPControlInput& zmp_vel) {
+DiscreteLIPDynamics::integrate(const LIPState& lip_state, const LIPControlInput& zmp_vel, const Eigen::Vector3d& disturbance) {
   Eigen::Vector3d nextStateX = updateState(lip_state, zmp_vel.x(), 0);
   Eigen::Vector3d nextStateY = updateState(lip_state, zmp_vel.y(), 1);
   Eigen::Vector3d nextStateZ = updateState(lip_state, zmp_vel.z(), 2);
   return LIPState(
-      Eigen::Vector3d(nextStateX(0), nextStateY(0), nextStateZ(0)),
-      Eigen::Vector3d(nextStateX(1), nextStateY(1), nextStateZ(1)),
-      Eigen::Vector3d(nextStateX(2), nextStateY(2), nextStateZ(2))
+      Eigen::Vector3d(nextStateX(0), nextStateY(0) + timestep_ * disturbance.x(), nextStateZ(0)),
+      Eigen::Vector3d(nextStateX(1), nextStateY(1) + timestep_ * disturbance.y(), nextStateZ(1)),
+      Eigen::Vector3d(nextStateX(2), nextStateY(2) + timestep_ * disturbance.z(), nextStateZ(2))
   );
 }
 
